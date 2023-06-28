a581f0966ddb509750db2cf864b8c98f1d57d49e
Manuel Odendahl <wesen@ruinwesen.com>
2023-06-28 14:44:56 -0400

    :arrow_up: Bump glazed and adapt to new APIs

--- cmd/cliopatra/cmds/ls.go
+++ cmd/cliopatra/cmds/ls.go
@@ -8,9 +8,9 @@ import (
 	"github.com/go-go-golems/glazed/pkg/cmds"
 	"github.com/go-go-golems/glazed/pkg/cmds/layers"
 	"github.com/go-go-golems/glazed/pkg/cmds/parameters"
-	"github.com/go-go-golems/glazed/pkg/middlewares/table"
 	"github.com/go-go-golems/glazed/pkg/processor"
 	"github.com/go-go-golems/glazed/pkg/settings"
+	"github.com/go-go-golems/glazed/pkg/types"
 	"github.com/spf13/cobra"
 	"strings"
 )
@@ -27,7 +27,7 @@ func (l *LsProgramCommand) Run(
 	ctx context.Context,
 	parsedLayers map[string]*layers.ParsedParameterLayer,
 	ps map[string]interface{},
-	gp processor.Processor,
+	gp processor.TableProcessor,
 ) error {
 	repositories, ok := ps["repository"].([]string)
 	if !ok {
@@ -40,19 +40,17 @@ func (l *LsProgramCommand) Run(
 		return err
 	}
 
-	gp.OutputFormatter().AddTableMiddlewareInFront(table.NewReorderColumnOrderMiddleware([]string{"name", "desc", "args"}))
-
 	for _, program := range r.GetPrograms() {
 		ps_, err2 := program.ComputeArgs(map[string]interface{}{})
 		if err2 != nil {
 			return err2
 		}
-		obj := map[string]interface{}{
-			"name": program.Name,
-			"desc": program.Description,
-			"args": strings.Join(ps_, " "),
-		}
-		err := gp.ProcessInputObject(ctx, obj)
+		obj := types.NewRow(
+			types.MRP("name", program.Name),
+			types.MRP("desc", program.Description),
+			types.MRP("args", strings.Join(ps_, " ")),
+		)
+		err := gp.AddRow(ctx, obj)
 		if err != nil {
 			return err
 		}
